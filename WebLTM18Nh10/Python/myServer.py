70683b4667049e9cab47f86f5d4115792d0cb414
Rysh <nghiathinh2000@gmail,com>
2021-11-29 15:44:56 +0700

    feat: Add try/except server error

--- WebLTM18Nh10/Python/myServer.py
+++ WebLTM18Nh10/Python/myServer.py
@@ -8,19 +8,20 @@ def openServer(localIP, localPort):
     UDPServerSocket.bind((localIP, localPort))
     print('Opening server...')
     return UDPServerSocket
-
 def start_running():
-    myServer = openServer(localIP, localPort)
-    path = "C:/LTM_WEBAPPS/MyLoginDB SQLite.db"
-    myQ = MyQueue(mysql_path=path)
-    myQ.load_weight("convlstm3d_128_16.pth")
-    myQ.startThread()
-    while (True):
-        bytesAddressPair = myServer.recvfrom(1024)
-        ma_session = bytesAddressPair[0].decode()
-        print(f"Ma Session: {ma_session}")
-        myQ.putItem(ma_session)
-        myQ.joinQueue()
-
+    try:
+        myServer = openServer(localIP, localPort)
+        path = "G:/NAM 4/LTM/eclipse/MyLoginDB SQLite.db"
+        myQ = MyQueue(mysql_path=path)
+        myQ.load_weight("Violence_video/src/weights/convlstm3d_128_16.pth")
+        myQ.startThread()
+        while (True):
+            bytesAddressPair = myServer.recvfrom(1024)
+            ma_session = bytesAddressPair[0].decode()
+            print(f"Ma Session: {ma_session}")
+            myQ.putItem(ma_session)
+            myQ.joinQueue()
+    except BaseException as error:
+        print(error)
 if __name__ == '__main__':
     start_running()
